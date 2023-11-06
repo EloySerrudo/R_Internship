2ac466a003ff67dfd139e6275e0850608a48322e
Eloy Ricardo Serrudo Careaga <42912680+EloySerrudo@users.noreply.github.com>
2023-11-06 16:08:53 +0100

    Update Zika-Upsetplot.R
    Update Zika-Upsetplot.R

--- Zika-Upsetplot.R
+++ Zika-Upsetplot.R
@@ -49,10 +49,9 @@ rm(cols.num)
 
 # Extracting Hits ---------------------------------------------------------
 
-
 Hits.Savidis <- dataSavidis[,-2]
 Hits.Li <- dataLi[1:700, -(2:4)]
-Hits.Dukhovny <- dataDukhovny[1:700, -(1:14)]
+Hits.Dukhovny <- dataDukhovny[1:700, -c(1, 3:15)]
 Hits.WangGSC <- dataWangGSC[,-(2:3)]
 Hits.Wang293FT <- dataWang293FT[,-(2:5)]
 Hits.Rother <- dataRother[,-(2:6)]
@@ -106,7 +105,7 @@ genes.DF.int |>
 
 gsea <- gost(query = genes.int, organism = "hsapiens", sources = c("GO"), evcodes = T)
 gseaUp <- gsea$result |> filter(term_size < 500, term_size > 10)
-write_xlsx(gseaUp, "GSEAup_Zika.xlsx")
+write_xlsx(gseaUp, "ZIKVData/GSEAup_Zika.xlsx")
 
 gostplot(gsea, interactive = FALSE)
 
