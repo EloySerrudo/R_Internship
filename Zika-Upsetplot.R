e06fd0f129bf64d0c0927c1906ca237e8465372e
Eloy Ricardo Serrudo Careaga <42912680+EloySerrudo@users.noreply.github.com>
2023-11-06 13:27:21 +0100

    Complete Update Zika-Upsetplot.R
    Complete Update Zika-Upsetplot.R

--- Zika-Upsetplot.R
+++ Zika-Upsetplot.R
@@ -1,24 +1,30 @@
 library(tidyverse)
 library(readxl)
+library(writexl)
 library(ComplexUpset)
 library(gprofiler2)
 
-# Dukhovny (2019) Los CSV no estan incluidos en la tabla ¿xq?
-#my_data <- read.table(file = '1-s2-0.txt', sep = ',', header = FALSE)
-#colnames(my_data) <- c("Gene ID","MAGeCK1",	"MAGeCK2",	"RIGER 1",	"RIGER 2")
 
 # Loading Datasets --------------------------------------------------------
 
 dataSavidis <- read_excel("ZKVData/1-s2.0-S2211124716307689-mmc8.xlsx", 
                           sheet = "Summary CME Compare")[-1,]
 
-dataLi <- read_excel("ZKVData/pnas.1900867116.sd01.xlsx", 
-                     sheet = "zika_gw_5th_best")
+dataLi <- read_excel("ZKVData/pnas.1900867116.sd01.xlsx", sheet = "zika_gw_5th_best")
 colnames(dataLi) <- as.vector(dataLi[1,])
 dataLi <- dataLi[-1,]
 cols.num <- c("sgRNAs", "zika.init.5th", "p.bh")
 dataLi[cols.num] <- sapply(dataLi[cols.num],as.numeric)
 dataLi <- dataLi[order(dataLi$p.bh),]
+
+dataDukhovny <- read.csv("ZKVData/JVI.00211-19-sd003.csv", header=TRUE, 
+                         sep = "\t", stringsAsFactors=FALSE)
+dataDukhovny <- dataDukhovny |> 
+  mutate(Gene = ifelse(grepl("^[A-Z0-9]+$", id), id, str_extract(id, "\\(([^)]+)\\)$")), 
+         .after = id) |> 
+  mutate(Gene = gsub("\\(|\\)", "", Gene)) |> 
+  arrange(pos.rank) |> 
+  filter(!is.na(Gene))
 
 dataWangGSC <- read_excel("ZKVData/NIHMS1553325-supplement-2.xlsx", 
                           sheet = "Ranking", col_names = FALSE, skip = 1)
@@ -36,7 +42,7 @@ dataRother <- read_excel("ZKVData/1-s2.0-S0168170221000459-mmc1.xlsx",
 dataShue <- read_excel("ZKVData/jvi.00596-21-s0001.xls", sheet = "Genelist")
 cols.num <- c("deseq2.FC", "deseq2.pval", "mageck.rank.pos", "mageck.fdr.pos", 
               "mageck.rank.neg", "mageck.fdr.neg")
-dataShue[cols.num] <- sapply(dataShue[cols.num],as.numeric)
+dataShue[cols.num] <- sapply(dataShue[cols.num], as.numeric)
 dataShue <- dataShue |> arrange(deseq2.pval, desc(deseq2.FC))
 rm(cols.num)
 
@@ -46,45 +52,31 @@ rm(cols.num)
 
 Hits.Savidis <- dataSavidis[,-2]
 Hits.Li <- dataLi[1:700, -(2:4)]
+Hits.Dukhovny <- dataDukhovny[1:700, -(1:14)]
 Hits.WangGSC <- dataWangGSC[,-(2:3)]
 Hits.Wang293FT <- dataWang293FT[,-(2:5)]
 Hits.Rother <- dataRother[,-(2:6)]
 Hits.Shue <- dataShue[1:700,-(2:7)]
 
-rm(dataSavidis, dataLi, dataWangGSC, dataWang293FT, dataRother, dataShue)
-
-genes <- c(unlist(Hits.Li), unlist(Hits.Rother), unlist(Hits.Savidis), 
+genes <- c(unlist(Hits.Li), unlist(Hits.Rother), Hits.Dukhovny, unlist(Hits.Savidis), 
            unlist(Hits.Shue), unlist(Hits.WangGSC), unlist(Hits.Wang293FT))
 genes <- genes |> unique() |> sort()
 
 genes.DF <- data.frame(genes, 
                        genes %in% unlist(Hits.Savidis), 
                        genes %in% unlist(Hits.Li), 
+                       genes %in% Hits.Dukhovny,
                        genes %in% unlist(Hits.WangGSC), 
                        genes %in% unlist(Hits.Wang293FT), 
                        genes %in% unlist(Hits.Rother), 
                        genes %in% unlist(Hits.Shue),
                        row.names = NULL)
-colnames(genes.DF)[-1] <- c("Savidis", "Li", "Wang.GSC", "Wang.293FT", "Rother", "Shue")
+colnames(genes.DF)[-1] <- c("Savidis", "Li", "Dukhovny", "Wang.GSC", "Wang.293FT", "Rother", "Shue")
 papers <- colnames(genes.DF)[-1]
+
 
 # Plotting the Upset plot -------------------------------------------------
 
-
-upset(
-  data = genes.DF,
-  intersect = papers,
-  name = 'Papers', 
-  width_ratio=0.1
-)
-
-upset(
-  data = genes.DF,
-  intersect = papers,
-  name = 'Papers', 
-  width_ratio=0.1, 
-  mode='inclusive_intersection'
-)
 
 upset(
   data = genes.DF,
@@ -95,13 +87,14 @@ upset(
   min_degree=2
 )
 
+
+# Selecting Groups --------------------------------------------------------
+
 genes.DF.int <- genes.DF |> 
   rowwise() |>
   mutate(Intersection = sum(c_across(2:7))) |> 
   filter(Intersection > 1)
 
-
-# Selecting Groups --------------------------------------------------------
 genes.int <- unlist(genes.DF.int$genes, use.names = FALSE)
 
 genes.DF.int |> 
@@ -111,42 +104,21 @@ genes.DF.int |>
 
 # Gen set enrichment analysis ---------------------------------------------
 
-input <- genes.int
-gsea <- gost(query = input, organism = "hsapiens", 
-             sources = c("GO"), evcodes = T)
-gseaUp <- filter(gsea$result, term_size < 500, term_size > 10)
+gsea <- gost(query = genes.int, organism = "hsapiens", sources = c("GO"), evcodes = T)
+gseaUp <- gsea$result |> filter(term_size < 500, term_size > 10)
+write_xlsx(gseaUp, "GSEAup_Zika.xlsx")
+
+gostplot(gsea, interactive = FALSE)
+
 
 
 
 # Not important -----------------------------------------------------------
 
+dataDukhovny1 <- read.csv("inData/JVI.00211-19-sd003.csv", header=TRUE, 
+                        sep = "\t", stringsAsFactors=FALSE)
+dataDukhovny2 <- read.csv("inData/JVI.00211-19-sd004.csv", header=TRUE, 
+                          sep = "\t", stringsAsFactors=FALSE)
 
 #https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7859841/
 #https://bioconductor.org/packages/release/data/experiment/html/airway.html
-
-
-a <- dataDukhovny |> 
-  mutate(Gene = str_extract(id, "\\(([^)]+)\\)")) |> 
-  mutate(Gene = ifelse(!is.na(Gene), gsub("\\(|\\)", "", Gene), 
-                       ifelse(grepl("^[A-Z0-9]+$", id), id, NA)))
-
-a$id[is.na(a$Gene)]
-
-sum(!grepl("^[A-Z0-9]+$", dataDukhovny$Gene))
-
-
-
-
-dataDukhovny <- dataDukhovny |> 
-  filter(grepl("^[A-Z0-9]+$", dataDukhovny$Gene))
-
-dataDukhovny <- read.csv("inData/JVI.00211-19-sd003.csv", header=TRUE, sep = "\t", stringsAsFactors=FALSE)
-dataDukhovny <- dataDukhovny |> arrange(pos.rank)
-dataDukhovny <- dataDukhovny[1:500,]
-dataDukhovny <- dataDukhovny |> 
-  mutate(Gene = str_extract(id, "\\(([^)]+)\\)")) |> 
-  mutate(Gene = ifelse(!is.na(Gene), gsub("\\(|\\)", "", Gene), 
-                       ifelse(grepl("^[A-Z0-9]+$", id), id, NA))) |> 
-  filter(!is.na(Gene), grepl("^[A-Z0-9]+$", dataDukhovny$Gene))
-
-Hits.Dukhovny <- as.data.frame(dataDukhovny[, -(1:14)])
