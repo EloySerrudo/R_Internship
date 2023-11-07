25e593417e280e093c8098f0a2483b8266d337f8
Eloy Ricardo Serrudo Careaga <42912680+EloySerrudo@users.noreply.github.com>
2023-11-07 17:00:53 +0100

    Update Zika-Upsetplot.R
    Update Zika-Upsetplot.R

--- Zika-Upsetplot.R
+++ Zika-Upsetplot.R
@@ -49,13 +49,14 @@ rm(cols.num)
 
 # Extracting Hits ---------------------------------------------------------
 
+
 Hits.Savidis <- dataSavidis[,-2]
-Hits.Li <- dataLi[1:700, -(2:4)]
-Hits.Dukhovny <- dataDukhovny[1:700, -c(1, 3:15)]
+Hits.Li <- dataLi[1:500, -(2:4)]
+Hits.Dukhovny <- dataDukhovny[1:500, -c(1, 3:15)]
 Hits.WangGSC <- dataWangGSC[,-(2:3)]
 Hits.Wang293FT <- dataWang293FT[,-(2:5)]
 Hits.Rother <- dataRother[,-(2:6)]
-Hits.Shue <- dataShue[1:700,-(2:7)]
+Hits.Shue <- dataShue[1:500,-(2:7)]
 
 genes <- c(unlist(Hits.Li), unlist(Hits.Rother), Hits.Dukhovny, unlist(Hits.Savidis), 
            unlist(Hits.Shue), unlist(Hits.WangGSC), unlist(Hits.Wang293FT))
@@ -97,20 +98,78 @@ genes.DF.int <- genes.DF |>
 genes.int <- unlist(genes.DF.int$genes, use.names = FALSE)
 
 genes.DF.int |> 
-  filter(Li & Wang.GSC & Wang.293FT & Rother & Savidis & Shue) |> 
+  filter(Li & Wang.293FT & Rother & Savidis & Shue) |> 
   select(genes) |> 
   unlist(use.names = FALSE)
 
 # Gen set enrichment analysis ---------------------------------------------
 
+# ALL GENES
 gsea <- gost(query = genes.int, organism = "hsapiens", sources = c("GO"), evcodes = T)
 gseaUp <- gsea$result |> filter(term_size < 500, term_size > 10)
 write_xlsx(gseaUp, "ZIKVData/GSEAup_Zika.xlsx")
 
 gostplot(gsea, interactive = FALSE)
 
+GO.MF <- gseaUp$intersection[20:22]
+
+# SHUE GENES
+gseaShue <- gost(query = Hits.Shue$genes, organism = "hsapiens", sources = c("GO"), evcodes = T)
+gseaUpShue <- gseaShue$result |> filter(term_size < 500, term_size > 10)
 
 
 
-#https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7859841/
-#https://bioconductor.org/packages/release/data/experiment/html/airway.html
+
+
+GO.BP <- unique(str_split(paste(gseaUp$intersection[1:12], collapse = ","), ",")[[1]])
+GO.BP
+
+GO.CC <- unique(str_split(paste(gseaUp$intersection[13:19], collapse = ","), ",")[[1]])
+GO.CC
+
+GO.MF <- unique(str_split(paste(gseaUp$intersection[20:22], collapse = ","), ",")[[1]])
+GO.MF
+
+genes2 <- sort(unique(c(GO.BP, GO.CC, GO.MF)))
+
+genes2.DF <- data.frame(genes2, 
+                       genes2 %in% GO.BP, 
+                       genes2 %in% GO.CC, 
+                       genes2 %in% GO.MF,
+                       row.names = NULL)
+colnames(genes2.DF)[-1] <- c("GO.BP", "GO.CC", "GO.MF")
+gen.set <- colnames(genes2.DF)[-1]
+
+upset(
+  data = genes2.DF,
+  intersect = gen.set,
+  name = 'gen.set', 
+  width_ratio=0.3, 
+  mode='inclusive_intersection',
+)
+
+BiocManager::install(c("clusterProfiler", "enrichplot", "DOSE"))
+
+library(clusterProfiler)
+library(enrichplot)
+library(DOSE)
+
+# modify the g:Profiler data frame
+gp_mod <- gseaUp[,c("query", "source", "term_id",
+                            "term_name", "p_value", "query_size", 
+                            "intersection_size", "term_size", 
+                            "effective_domain_size", "intersection")]
+gp_mod$GeneRatio = paste0(gp_mod$intersection_size,  "/", gp_mod$query_size)
+gp_mod$BgRatio = paste0(gp_mod$term_size, "/", gp_mod$effective_domain_size)
+
+names(gp_mod) = c("Cluster", "Category", "ID", "Description", "p.adjust", 
+                  "query_size", "Count", "term_size", "effective_domain_size", 
+                  "geneID", "GeneRatio", "BgRatio")
+gp_mod$geneID = gsub(",", "/", gp_mod$geneID)
+#row.names(gp_mod) = gp_mod$ID
+
+# define as compareClusterResult object
+gp_mod_cluster = new("compareClusterResult", compareClusterResult = gp_mod)
+
+# define as enrichResult object
+gp_mod_enrich  = new("enrichResult", result = gp_mod)
